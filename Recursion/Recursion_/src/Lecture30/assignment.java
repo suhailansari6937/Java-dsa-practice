c01ca3bd1927a1b5d99c443033dcf716d860c319
Suhail <suhailansari.sa354@gmail.com>
2025-10-28 20:10:49 +0530

    Recursion added

--- Recursion/Recursion_/src/Lecture30/assignment.java
+++ Recursion/Recursion_/src/Lecture30/assignment.java
@@ -1,6 +1,14 @@
 package Lecture30;
 
 public class assignment {
+    static int question2(int n, int m){
+        if(m==0){
+            return 0;
+        }
+        int sum = question2(question2(n,m-1),1);
+        return sum;
+
+    }
     static void ques1(int n ){
         if(n<=0){
             System.out.println(n);
@@ -12,7 +20,8 @@ public class assignment {
 
     }
     static void main(String[] args) {
-        ques1(10);
+//        ques1(10);
+        question2(3,2);
 
 
     }
